b53bc7acb3f45872967c2b0d28fabf411aa656b2
dbenton93 <drbenton@calpoly.edu>
2013-05-07 14:02:51 -0700

    Midterm up

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -1,4 +1,5 @@
 import numpy as py
+import math
 
 def pointPotential(x,y,q,posx,posy):
     """Computes the electric potential from charge q at posx,posy and returns the potential at x,y"""
@@ -10,3 +11,8 @@ def pointPotential(x,y,q,posx,posy):
 def dipolePotential(x,y,q,d):
      return pointPotential(x,y,q,-d,0) + pointPotential(x,y,-q,d,0)
 
+def pointField(x,y,q,Xq,Yq):
+    """Takes arrays, x and y, charge q, position Xq, Yq and returns components of electric field in the form (Ex,Ey)
+    Ex = (k*q*(x-Xq))/(sqrt((x-Xq)**2+(y-Yq)**2))
+    Ey = (k*q*(y-Yq))/(sqrt((x-Xq)**2+(y-Yq)**2))
+    return (Ex,Ey)
